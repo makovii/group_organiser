0803968bd223c87434f70b4c1ca1e65e99dbd959
Makov-Vik <viktorkaktysikov@gmail.com>
2023-03-01 00:19:59 +0200

    added err check for router.RUn

--- server/server.go
+++ server/server.go
@@ -11,5 +11,8 @@ func Init() {
 	
 	cfg := config.GetConfig()
 
-	router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
+	err := router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
+	if err != nil {
+		return 
+	}
  }
