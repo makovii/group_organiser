d90065ba55ee976099112f71a7911dd1fab6e826
Makov-Vik <viktorkaktysikov@gmail.com>
2023-03-01 00:00:11 +0200

    added user api

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,13 +1,8 @@
 package middleware
 
 import (
-	// "fmt"
 	"github.com/gin-gonic/gin"
-	// "github.com/golang-jwt/jwt/v4"
-	// "github.com/ip-05/quizzus/config"
 	"net/http"
-	// "strings"
-	// "time"
 )
 
 type AuthedUser struct {
@@ -16,48 +11,13 @@ type AuthedUser struct {
 	Email          string `json:"email"`
 }
 
-func AuthMiddleware(/*cfg *config.Config*/) gin.HandlerFunc {
+func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		header := c.GetHeader("Authorization")
 		if header == "" {
 			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Missing `Authorization` header."})
 			return
 		}
-
-		// tokenString := strings.Split(header, " ")[1]
-
-		// secret := []byte(cfg.Secrets.Jwt)
-		// token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		// 		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		// 	}
-
-		// 	return secret, nil
-		// })
-
-		// if err != nil {
-		// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid token."})
-		// 	return
-		// }
-
-		// if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		// 	exp := int64(claims["exp"].(float64))
-		// 	now := time.Now().Unix()
-		// 	if now > exp {
-		// 		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Expired token."})
-		// 		return
-		// 	}
-
-			// authedUser := AuthedUser{
-			// 	Id:             claims["id"].(string),
-			// 	Name:           claims["name"].(string),
-			// 	Email:          claims["email"].(string),
-			// }
-		// 	c.Set("authedUser", authedUser)
-		// 	c.Next()
-		// } else {
-		// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid token."})
-		// }
 
 		c.Set("authedUser", "lfkfkfkfk")
 		c.Next()
