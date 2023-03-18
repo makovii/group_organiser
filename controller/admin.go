78c8c1d6917c6e310a9db413d5b9bda51fbf68e5
Alexey <lesha3320@gmail.com>
2023-03-18 20:43:31 +0200

    Add admin.go

---
+++ controller/admin.go
@@ -0,0 +1,40 @@
+package cotroller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type AdminController struct {
+	DB *gorm.DB
+}
+
+func NewAdminController(db *gorm.DB) *AdminController {
+	return &AdminController{DB: db}
+}
+
+func (u UserController) GetAdmin(c *gin.Context) {
+	admin := "admin1"
+	c.JSON(http.StatusOK, admin)
+	return
+}
+
+func (u UserController) GetById(c *gin.Context) {
+	GetById := "getId"
+	c.JSON(http.StatusOK, GetById)
+	return
+}
+
+func (u UserController) BanById(c *gin.Context) {
+	BanById := "banId"
+	c.JSON(http.StatusOK, BanById)
+	return
+}
+
+func (u UserController) GetTeans(c *gin.Context) {
+	GetTeams := "teams1"
+	c.JSON(http.StatusOK, GetTeams)
+	return
+}
