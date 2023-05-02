cdd2ea4307676c9a240e146ad68c8cebfc1b89d8
Alexey <lesha3320@gmail.com>
2023-05-02 16:05:28 +0300

    added admin logic

--- controller/admin.go
+++ controller/admin.go
@@ -5,32 +5,62 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/makovii/group_organiser/config"
 	"github.com/makovii/group_organiser/database"
 	"gorm.io/gorm"
 )
 
 type AdminController struct {
-	DB *gorm.DB
+	DB  *gorm.DB
+	CFG *config.Config
 }
 
-func NewAdminController(db *gorm.DB) *AdminController {
-	return &AdminController{DB: db}
+func NewAdminController(db *gorm.DB, cfg *config.Config) *AdminController {
+	return &AdminController{DB: db, CFG: cfg}
 }
 
-func (a *AdminController) GetAdmin(c *gin.Context) {
-	var admin database.User
-	c.JSON(http.StatusOK, admin)
+func (a *AdminController) GetAdmins(c *gin.Context) {
+	var admins []database.User
+
+	a.DB.Where("role = ?", a.CFG.Role.AdminId).Find(&admins)
+
+	if len(admins) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Admins"})
+		return
+	}
+
+	c.JSON(http.StatusOK, admins)
 
 }
 
-func (a *AdminController) GetById(c *gin.Context) {
-	var GetById database.User
-	c.JSON(http.StatusOK, GetById)
+func (a *AdminController) GetUserById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Query("id"))
+	var user database.User
+
+	if a.DB.Where("id = ?", id).First(&user) != nil {
+		c.JSON(http.StatusOK, user)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+	}
+}
+
+func (a *AdminController) GetTeamById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Query("id"))
+	var teams database.Team
+
+	if a.DB.Where("id = ?", id).First(&teams) != nil {
+		c.JSON(http.StatusOK, teams)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Team not found"})
+	}
 
 }
 
 func (a *AdminController) BanById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Query("id"))
+
 	var BanById database.User
+	a.DB.Where("id = ?", id).First(&BanById)
 	BanById.Ban = true
 	c.JSON(http.StatusOK, BanById)
 
@@ -52,7 +82,7 @@ func (a *AdminController) AcceptManagerRegistration(c *gin.Context) {
 	var manager database.User
 	a.DB.Where("id = ?", id).First(&manager)
 
-	manager.Ban = false;
+	manager.Ban = false
 
 	a.DB.Save(&manager)
 	c.JSON(http.StatusOK, manager)
