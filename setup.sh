a37eb2250f59750ebb9ea3d1b39f260cdc88942c
makovii <viktorkaktysikov@gmail.com>
2023-05-20 23:10:12 +0300

    fixed heroku deploy

--- setup.sh
+++ setup.sh
@@ -4,29 +4,35 @@ secure = false
 domain = "localhost"
 base = "http://localhost:1234"
 host = "localhost"
-port = 8080"  >> ~/config/config.toml
+port = 8080
+"  >> ~/config/config.toml
 
 echo "[admin]
-admin_id = 0"  >> ~/config/config.toml
+admin_id = 0
+"  >> ~/config/config.toml
 
 echo "[status]
 waitId = 1
 acceptId = 2
 rejectId = 3
-cancelId = 4" >> ~/config/config.toml
+cancelId = 4
+" >> ~/config/config.toml
 
 echo "[type]
 registrationId = 1
 JoinTeamId = 2
-LeaveTeamId = 3" >> ~/config/config.toml
+LeaveTeamId = 3
+" >> ~/config/config.toml
 
 echo "[role]
 adminId = 1
 managerId = 2
-playerId = 3" >> ~/config/config.toml
+playerId = 3
+" >> ~/config/config.toml
 
 echo "[secrets]
-jwt = "secret"" >> ~/config/config.toml
+jwt = "secret"
+" >> ~/config/config.toml
 
 echo "[db]
 host = "localhost"
@@ -34,4 +40,5 @@ port = 5432
 user = "perceval"
 password = "password"
 name = "group_organiser"
-secure = false" >> ~/config/config.toml
+secure = false
+" >> ~/config/config.toml
