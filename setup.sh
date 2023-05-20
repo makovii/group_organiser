f8b77a89d4e4d1fb4725bfe368543d701ceb0b37
makovii <viktorkaktysikov@gmail.com>
2023-05-20 23:02:26 +0300

    fixed heroku deploy

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
-mkdir -p ~/.streamlit/config/
+mkdir -p ~/config/
 echo "[server]
 secure = false
 domain = "localhost"
 base = "http://localhost:1234"
 host = "localhost"
-port = 8080"  >> ~/.streamlit/config/config.toml
+port = 8080"  >> ~/config/config.toml
 
 echo "[admin]
-admin_id = 0"  >> ~/.streamlit/config/config.toml
+admin_id = 0"  >> ~/config/config.toml
 
 echo "[status]
 waitId = 1
 acceptId = 2
 rejectId = 3
-cancelId = 4" >> ~/.streamlit/config/config.toml
+cancelId = 4" >> ~/config/config.toml
 
 echo "[type]
 registrationId = 1
 JoinTeamId = 2
-LeaveTeamId = 3" >> ~/.streamlit/config/config.toml
+LeaveTeamId = 3" >> ~/config/config.toml
 
 echo "[role]
 adminId = 1
 managerId = 2
-playerId = 3" >> ~/.streamlit/config/config.toml
+playerId = 3" >> ~/config/config.toml
 
 echo "[secrets]
-jwt = "secret"" >> ~/.streamlit/config/config.toml
+jwt = "secret"" >> ~/config/config.toml
 
 echo "[db]
 host = "localhost"
@@ -34,4 +34,4 @@ port = 5432
 user = "perceval"
 password = "password"
 name = "group_organiser"
-secure = false" >> ~/.streamlit/config/config.toml
+secure = false" >> ~/config/config.toml
