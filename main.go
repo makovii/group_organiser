c848a6442c9c924fffff27a5ba8512fde643b6b5
makovii <viktorkaktysikov@gmail.com>
2023-05-24 11:28:51 +0300

    try fix port heroku

--- main.go
+++ main.go
@@ -15,17 +15,21 @@ func main() {
 	cfg := config.GetConfig()
 
 	portStr := os.Getenv("PORT")
-	portInt64 := cfg.Server.Port
-	if portStr != "" {
-		var err error
-			portInt64, err = strconv.ParseInt(portStr, 10, 64) // Default port if not specified
-			if err != nil {
-				fmt.Println("Error during conversion")
-				return
-			}
+	portInt64, err := strconv.ParseInt(portStr, 10, 64) // Default port if not specified
+	if err != nil {
+		fmt.Println("Error during conversion")
 	}
+	// portInt64 := cfg.Server.Port
+	// if portStr != "" {
+	// 	var err error
+	// 		portInt64, err = strconv.ParseInt(portStr, 10, 64) // Default port if not specified
+	// 		if err != nil {
+	// 			fmt.Println("Error during conversion")
+	// 			return
+	// 		}
+	// }
 
-	err := router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, portInt64))
+	err = router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, portInt64))
 	if err != nil {
 		fmt.Println("Smth wrong witn router.Run function: ", err)
 	}
