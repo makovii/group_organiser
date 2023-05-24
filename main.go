d137f1498fc4716b1bcd80afb43248e68dc19e34
makovii <viktorkaktysikov@gmail.com>
2023-05-24 11:49:25 +0300

    try fix port heroku

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/joho/godotenv"
 	"github.com/makovii/group_organiser/config"
 	"github.com/makovii/group_organiser/server"
 )
@@ -14,20 +15,22 @@ func main() {
 
 	cfg := config.GetConfig()
 
-	portStr := os.Getenv("PORT")
-	portInt64, err := strconv.ParseInt(portStr, 10, 64) // Default port if not specified
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Println("Error during conversion")
+    fmt.Print("Error loading .env file")
+  }
+
+	portStr := os.Getenv("PORT")
+
+	portInt64 := cfg.Server.Port
+	if portStr != "" {
+		var err error
+			portInt64, err = strconv.ParseInt(portStr, 10, 64) // Default port if not specified
+			if err != nil {
+				fmt.Println("Error during conversion")
+				return
+			}
 	}
-	// portInt64 := cfg.Server.Port
-	// if portStr != "" {
-	// 	var err error
-	// 		portInt64, err = strconv.ParseInt(portStr, 10, 64) // Default port if not specified
-	// 		if err != nil {
-	// 			fmt.Println("Error during conversion")
-	// 			return
-	// 		}
-	// }
 
 	err = router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, portInt64))
 	if err != nil {
