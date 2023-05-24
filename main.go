134649babc9a7b0ca1efbc6d607a073d3b1ca2b3
makovii <viktorkaktysikov@gmail.com>
2023-05-24 11:24:02 +0300

    reolace system  port to heroku port

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/makovii/group_organiser/server"
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/makovii/group_organiser/config"
+	"github.com/makovii/group_organiser/server"
 )
 
 func main() {
@@ -11,7 +14,18 @@ func main() {
 
 	cfg := config.GetConfig()
 
-	err := router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
+	portStr := os.Getenv("PORT")
+	portInt64 := cfg.Server.Port
+	if portStr != "" {
+		var err error
+			portInt64, err = strconv.ParseInt(portStr, 10, 64) // Default port if not specified
+			if err != nil {
+				fmt.Println("Error during conversion")
+				return
+			}
+	}
+
+	err := router.Run(fmt.Sprintf("%s:%d", cfg.Server.Host, portInt64))
 	if err != nil {
 		fmt.Println("Smth wrong witn router.Run function: ", err)
 	}
